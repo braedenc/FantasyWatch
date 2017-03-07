3a2566cd547268006cabc393f215f1058d6fd13b
braedenc <braedenchriske@gmail.com>
2017-03-06 18:14:43 -0600

    initial commit

---
+++ FantasyWatch/FantasyWatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FantasyWatch.Startup))]
+namespace FantasyWatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
